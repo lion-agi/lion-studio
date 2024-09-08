f1d57bbe6d694398a985095670585218c7f67c9a
OceanLi <122793010+ohdearquant@users.noreply.github.com>
2024-09-07 23:31:41 -0400

    updated console header

--- src/features/console/components/ConsoleHeader.jsx
+++ src/features/console/components/ConsoleHeader.jsx
@@ -17,10 +17,10 @@ const ConsoleHeader = () => {
 
   const navItems = [
     { name: 'Dashboard', path: '/console/dashboard', icon: LayoutDashboard },
+    { name: 'Workflow', path: '/console/workflow', icon: GitBranch },
+    { name: 'Library', path: '/console/library', icon: Library },
     { name: 'Integrations', path: '/console/Integrations', icon: LinkIcon },
     { name: 'Deployment', path: '/console/deployment', icon: Rocket },
-    { name: 'Library', path: '/console/library', icon: Library },
-    { name: 'Workflow', path: '/console/workflow', icon: GitBranch },
   ];
 
   return (
