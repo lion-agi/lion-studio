c34cb845832e9b83e533e2f5fb65275893214fcb
OceanLi <122793010+ohdearquant@users.noreply.github.com>
2024-09-09 13:54:08 -0400

    updated setting

--- src/features/admin/pages/Admin.jsx
+++ src/features/admin/pages/Admin.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { Tabs, TabsContent } from "@/common/components/ui/tabs";
 import TabSwitchBar from '@/common/components/TabSwitchBar';
 import UserRoles from '../components/UserRoles';
-import AccessLogs from '../components/AccessLogs';
+import AccessLogs from '@/common/components/AccessLogs';
 import SecuritySettings from '../components/SecuritySettings';
 import Secrets from '../components/Secrets';
 import { useAdminData } from '../useAdminData';
